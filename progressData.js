b4f5f337ef6710c1460118b951cf0a61f261754c
PauG.Cols <66697376+paulider@users.noreply.github.com>
2026-06-03 07:40:22 +0200

    Add progress chart sample data

---
+++ progressData.js
@@ -0,0 +1,22 @@
+export const progressMetrics = [
+  {
+    id: 'weight',
+    title: 'Pes corporal',
+    unit: 'kg',
+    values: [83.2, 82.8, 82.5, 82.1, 81.7, 81.4]
+  },
+  {
+    id: 'waist',
+    title: 'Cintura',
+    unit: 'cm',
+    values: [92, 91.5, 91, 90.4, 90, 89.5]
+  },
+  {
+    id: 'sessions',
+    title: 'Sessions setmanals',
+    unit: '',
+    values: [1, 2, 2, 3, 2, 3]
+  }
+];
+
+export const progressLabels = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6'];
